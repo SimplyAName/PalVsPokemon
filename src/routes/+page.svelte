ce948348e7424348c4fd9b2f45027de705f4de4b
SimplyAName <michael.loney137@gmail.com>
2024-02-04 23:00:28 +0000

    Basic page structure and poc of how it should function

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,38 @@
 <script lang="ts">
 	import { Button } from '$lib/components/ui/button';
+
+	let wins = 0;
+	let loses = 0;
+
+	let answer = 'PalWorld';
+
+	function randomizeAnswer() {
+		let randNum = Math.floor(Math.random() * 2);
+
+		switch (randNum) {
+			case 0:
+				answer = 'Pokémon';
+				break;
+
+			case 1:
+				answer = 'PalWorld';
+				break;
+
+			default:
+				answer = 'Unknown';
+				break;
+		}
+	}
+
+	function answerQuestion(submitted: String) {
+		if (submitted === answer) {
+			wins++;
+		} else {
+			loses++;
+		}
+
+		randomizeAnswer();
+	}
 </script>
 
 <h1 class="text-center text-3xl">Welcome to Pal Vs Pokémon</h1>
@@ -10,11 +43,23 @@
 
 <div id="gameWindow" class="flex flex-col items-center">
 	<div>
+		You have {wins} correct and {loses} wrong
+	</div>
+
+	<div>
 		<img src="https://placehold.co/600x400" />
 	</div>
 	<br />
 	<div id="buttons" class="flex flex-row gap-4">
-		<Button>PalWorld</Button>
-		<Button>Pokémon</Button>
+		<Button
+			on:click={() => {
+				answerQuestion('PalWorld');
+			}}>PalWorld</Button
+		>
+		<Button
+			on:click={() => {
+				answerQuestion('Pokémon');
+			}}>Pokémon</Button
+		>
 	</div>
 </div>
