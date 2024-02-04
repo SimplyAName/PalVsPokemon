6d0b0ec556031c29be28d1af74023913ee12544d
SimplyAName <michael.loney137@gmail.com>
2024-02-04 22:45:42 +0000

    Init layout idea plus setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+</script>
+
+<h1 class="text-center text-3xl">Welcome to Pal Vs Pokémon</h1>
+<p class="text-center">
+	Below will have either creature from Pokémon or PalWorld. Try and get the highest streak you can!
+</p>
+<br />
+
+<div id="gameWindow" class="flex flex-col items-center">
+	<div>
+		<img src="https://placehold.co/600x400" />
+	</div>
+	<br />
+	<div id="buttons" class="flex flex-row gap-4">
+		<Button>PalWorld</Button>
+		<Button>Pokémon</Button>
+	</div>
+</div>
