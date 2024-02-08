323290b849d69327f6a5cb7a4f680e2e31270596
SimplyAName <michael.loney137@gmail.com>
2024-02-08 01:34:27 +0000

    Add custom spinner animation

---
+++ src/lib/components/ui/animations/SquareFlipSpinner.svelte
@@ -0,0 +1,37 @@
+<script>
+	export let color = '#000000';
+	export let size = '100px';
+	export let duration = '3s';
+	export let background = '';
+</script>
+
+<div
+	class="square rounded-md shadow"
+	{color}
+	style="--size: {size}; --color: {color}; --duration: {duration};--background: {background}"
+/>
+
+<style>
+	.square {
+		width: var(--size);
+		height: var(--size);
+		background-color: var(--color);
+		animation: spinningAnimation var(--duration) 0s infinite ease-in;
+		background: var(--background);
+	}
+
+	@keyframes spinningAnimation {
+		25% {
+			transform: rotateX(180deg) rotateY(0);
+		}
+		50% {
+			transform: rotateX(180deg) rotateY(180deg);
+		}
+		75% {
+			transform: rotateX(0) rotateY(180deg);
+		}
+		100% {
+			transform: rotateX(0) rotateY(0);
+		}
+	}
+</style>
