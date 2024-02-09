2b5b116fcb72fd3fde79d42e26a41eec0d735de8
SimplyAName <michael.loney137@gmail.com>
2024-02-09 00:57:19 +0000

    Fix combo indicator location

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,8 @@
 
 	let wins = 0;
 	let loses = 0;
-	let streak = 0;
+	let streak = 4;
+	let bonusAlert: HTMLElement;
 
 	let waiting = false;
 
@@ -47,69 +48,91 @@
 		getRandCreature();
 	}
 
-	const textSize = spring(1, {
-		stiffness: 0.1,
-		damping: 1,
-		precision: 0.001
-	});
-
-	const zoomIn = setInterval(() => {
-		textSize.set(1.5);
-	}, 400);
-
-	const zoomOut = setInterval(() => {
-		textSize.set(1);
-	}, 800);
+	// while (streak >= 3) {
+	// 	bonusAlert.animate({});
+	// }
 </script>
 
 <div class="container flex h-screen flex-col justify-center">
-	<h1 class="text-center text-3xl">Welcome to Pal Vs Pokémon</h1>
-	<p class="text-center">
-		Below will have either creature from Pokémon or PalWorld. Try and get the highest streak you
-		can!
-	</p>
-	<br />
-
-	<div id="gameWindow" class="flex w-full flex-col items-center">
-		<div>
-			You have {wins} correct and {loses} wrong
-		</div>
-
-		<div class="flex h-96 flex-col justify-center">
-			{#if streak >= 3}
-				<!-- TODO: Have this hover over image at an angle. Make it work art style -->
-				<p transition:fade class="mt-4 block" style="transform: scale({$textSize});">
-					{streak} combo!!!
-				</p>
-			{/if}
-			{#if waiting}
-				<SquareFlipSpinner color="" background="linear-gradient(to bottom left, blue, pink)" />
-			{:else}
-				<img
-					alt="Creature to guess from. Starts with {answer?.name[0]}"
-					src={answer?.imageLink}
-					class="h-96 w-full"
-				/>
-			{/if}
-		</div>
-
+	<div class="rounded-md bg-white p-4">
+		<h1 class="text-center text-3xl">Welcome to Pal Vs Pokémon</h1>
+		<p class="text-center">
+			Below will have either creature from Pokémon or PalWorld. Try and get the highest streak you
+			can!
+		</p>
 		<br />
 
-		<div id="buttons" class="flex w-full flex-row gap-4 p-8">
-			<Button
-				class="h-16 w-full text-xl"
-				disabled={waiting}
-				on:click={() => {
-					answerQuestion('PalWorld');
-				}}><b>PalWorld</b></Button
-			>
-			<Button
-				class="h-16 w-full text-xl"
-				disabled={waiting}
-				on:click={() => {
-					answerQuestion('Pokémon');
-				}}><b>Pokémon</b></Button
-			>
+		<div id="gameWindow" class="flex w-full flex-col items-center">
+			<div>
+				You have {wins} correct and {loses} wrong
+			</div>
+
+			<div class="flex h-96 flex-col items-start">
+				{#if streak >= 3}
+					<!-- TODO: Have this hover over image at an angle. Make it work art style -->
+					<p
+						bind:this={bonusAlert}
+						transition:fade
+						class="bonus-streak-alert flex shrink justify-center text-red-500"
+					>
+						<b>{streak} combo!!!</b>
+					</p>
+				{/if}
+
+				<div class="h-96 w-full">
+					{#if waiting}
+						<div class="h-full w-full">
+							<SquareFlipSpinner
+								color=""
+								background="linear-gradient(to bottom left, blue, pink)"
+							/>
+						</div>
+					{:else}
+						<img
+							alt="Creature to guess from. Starts with {answer?.name[0]}"
+							src={answer?.imageLink}
+							class="h-96 w-full"
+						/>
+					{/if}
+				</div>
+			</div>
+
+			<br />
+
+			<div id="buttons" class="flex w-full flex-row gap-4 p-8">
+				<Button
+					class="h-16 w-full text-xl"
+					disabled={waiting}
+					on:click={() => {
+						answerQuestion('PalWorld');
+					}}><b>PalWorld</b></Button
+				>
+				<Button
+					class="h-16 w-full text-xl"
+					disabled={waiting}
+					on:click={() => {
+						answerQuestion('Pokémon');
+					}}><b>Pokémon</b></Button
+				>
+			</div>
 		</div>
 	</div>
 </div>
+
+<style>
+	.bonus-streak-alert {
+		animation: pulse 2s ease-in-out infinite;
+	}
+
+	@keyframes pulse {
+		0% {
+			transform: scale(1) rotate(-45deg) translate(-33%);
+		}
+		50% {
+			transform: scale(1.5) rotate(-45deg) translate(-33%);
+		}
+		100% {
+			transform: scale(1) rotate(-45deg) translate(-33%);
+		}
+	}
+</style>
