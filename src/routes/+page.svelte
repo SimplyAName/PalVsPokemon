6907f9ed9b1aa37523575ae29c7de4367eab7a35
SimplyAName <michael.loney137@gmail.com>
2024-02-13 00:12:40 +0000

    Updated logic to handle failing responses better

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,39 +1,50 @@
 <script lang="ts">
-	import { Button } from '$lib/components/ui/button';
 	import { SquareFlipSpinner } from '$lib/components/ui/animations/SquareFlipSpinner';
 	import { onMount } from 'svelte';
 	import { fade } from 'svelte/transition';
-	import { spring } from 'svelte/motion';
 
 	onMount(getRandCreature);
+
+	let dialogMessage = '';
 
 	let wins = 0;
 	let loses = 0;
 	let streak = 0;
+	let palworldHovered = false;
+	let pokemonHovered = false;
 	let bonusAlert: HTMLElement;
+	let dialog: any;
 
 	let waiting = false;
 
-	let answer: {
-		id: number;
-		name: string;
-		originGame: string;
-		imageLink: string;
-		createdAt: Date;
-	} | null = null;
+	let randCreaturePromise = getRandCreature();
+
+	let currCreature: Promise<any>;
+
+	function newRandCreature() {
+		randCreaturePromise = getRandCreature();
+	}
 
 	async function getRandCreature() {
 		waiting = true;
-		const response = await fetch('/api/question', {
+
+		const result = await fetch('/api/question', {
 			method: 'GET',
 			headers: {
 				'content-type': 'application/json'
 			}
 		});
 
-		answer = await response.json();
-
 		waiting = false;
+
+		if (result.ok) {
+			currCreature = await result.json();
+			return currCreature;
+		}
+
+		console.error(result);
+
+		throw new Error(`Failed to get questions! Status: ${result.status}`);
 	}
 
 	function answerPalWorld() {
@@ -45,7 +56,7 @@
 	}
 
 	function _answerQuestion(submitted: String) {
-		if (submitted === answer?.originGame) {
+		if (submitted === currCreature?.originGame) {
 			wins++;
 			streak++;
 		} else {
@@ -53,20 +64,15 @@
 			streak = 0;
 		}
 
-		getRandCreature();
+		newRandCreature();
 	}
-
-	let palworldHovered: boolean,
-		pokemonHovered = false;
-
-	// while (streak >= 3) {
-	// 	bonusAlert.animate({});
-	// }
 </script>
 
 <svelte:window
 	on:keydown|preventDefault={(event) => {
-		console.log(event.key);
+		if (waiting) {
+			return;
+		}
 
 		switch (event.key) {
 			case 'ArrowLeft':
@@ -81,14 +87,13 @@
 
 <div class="flex h-screen w-screen flex-col items-center justify-center">
 	<div class="fixed z-10 flex h-screen flex-col md:flex-row">
-		<button on:click={answerPalWorld}>
-			<!-- svelte-ignore a11y-img-redundant-alt -->
+		<button disabled={waiting} on:click={answerPalWorld}>
 			<img
 				draggable="false"
 				class:greyScale={pokemonHovered}
 				src="/images/background/Backgroundbg-left.webp"
-				alt="PalWorld section of background image"
-				class="peer/palword object-cover transition-all duration-300 ease-in-out hover:scale-110"
+				alt="Collection of PalWorld on the left of the battleground"
+				class="z-10 transition-all duration-300 ease-in-out hover:z-20 hover:scale-110"
 				on:mouseenter={() => {
 					palworldHovered = true;
 				}}
@@ -97,14 +102,14 @@
 				}}
 			/>
 		</button>
-		<!-- svelte-ignore a11y-img-redundant-alt -->
-		<button on:click={answerPokemon}>
+
+		<button disabled={waiting} on:click={answerPokemon}>
 			<img
 				draggable="false"
 				class:greyScale={palworldHovered}
 				src="/images/background/Backgroundbg-right.webp"
-				alt="Pokémon section of background image"
-				class="peer/pokemon transition-transform duration-300 ease-in-out hover:scale-110 peer-hover/palwordImage:scale-50"
+				alt="Collection of Pokémon on the right of the battleground"
+				class="z-10 transition-all duration-300 ease-in-out hover:z-20 hover:scale-110"
 				on:mouseenter={() => {
 					pokemonHovered = true;
 				}}
@@ -115,11 +120,12 @@
 		</button>
 	</div>
 
-	<div class="z-20 flex flex-col items-center">
+	<div
+		class="z-50 flex aspect-square w-fit flex-col items-center bg-gradient-radial from-white to-transparent to-80% p-8"
+	>
 		<div>
 			You have {wins} correct and {loses} wrong
 		</div>
-		<!-- <div class="flex h-96 flex-col items-start"> -->
 		{#if streak >= 3}
 			<!-- TODO: Have this hover over image at an angle. Make it work art style -->
 			<p
@@ -131,19 +137,18 @@
 			</p>
 		{/if}
 
-		<div class="h-96 w-fit">
-			{#if waiting}
-				<div class="h-full w-full">
-					<SquareFlipSpinner color="" background="linear-gradient(to bottom left, blue, pink)" />
-				</div>
-			{:else}
+		<div class="flex h-[33dvh] w-fit flex-col justify-center">
+			{#await randCreaturePromise}
+				<SquareFlipSpinner color="" background="linear-gradient(to bottom left, blue, pink)" />
+			{:then creature}
 				<img
-					alt="Creature to guess from. Starts with {answer?.name[0]}"
-					src={answer?.imageLink}
-					class="h-96 w-fit"
+					alt="Creature to guess from. Starts with {creature?.name[0]}"
+					src={creature?.imageLink}
+					class="h-full w-fit"
 				/>
-			{/if}
-			<!-- </div> -->
+			{:catch error}
+				<p style="color: red">{error.message}</p>
+			{/await}
 		</div>
 	</div>
 </div>
