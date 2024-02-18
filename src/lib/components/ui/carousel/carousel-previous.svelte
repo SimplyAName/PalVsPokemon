dfc935a50f5c48c17225a8afd82b6c932ab31b81
SimplyAName <michael.loney137@gmail.com>
2024-02-18 02:37:14 +0000

    Added Carousel component

---
+++ src/lib/components/ui/carousel/carousel-previous.svelte
@@ -0,0 +1,31 @@
+<script>
+	import { Button, buttonVariants } from "$lib/components/ui/button/index.js";
+	import { cn } from "$lib/utils.js";
+	import { ArrowLeft } from "lucide-svelte";
+	import { getEmblaContext } from "./context.js";
+	let className = undefined;
+	export { className as class };
+	export let variant = "outline";
+	export let size = "icon";
+	const { orientation, canScrollPrev, scrollPrev, handleKeyDown } =
+		getEmblaContext("<Carousel.Previous/>");
+</script>
+
+<Button
+	{variant}
+	{size}
+	class={cn(
+		"absolute h-8 w-8 rounded-full touch-manipulation",
+		$orientation === "horizontal"
+			? "-left-12 top-1/2 -translate-y-1/2"
+			: "-top-12 left-1/2 -translate-x-1/2 rotate-90",
+		className
+	)}
+	disabled={!$canScrollPrev}
+	on:click={scrollPrev}
+	on:keydown={handleKeyDown}
+	{...$$restProps}
+>
+	<ArrowLeft class="h-4 w-4" />
+	<span class="sr-only">Previous slide</span>
+</Button>
