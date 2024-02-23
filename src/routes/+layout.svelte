e28d3893b59b5d5e5fed45c6ec3c4408894f09af
SimplyAName <michael.loney137@gmail.com>
2024-02-23 23:49:20 +0000

    Added analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,10 @@
 <script>
 	import '../app.pcss';
 	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+	import { inject } from '@vercel/analytics';
+	import { dev } from '$app/environment';
 
+	inject({ mode: dev ? 'development' : 'production' });
 	injectSpeedInsights();
 </script>
 
