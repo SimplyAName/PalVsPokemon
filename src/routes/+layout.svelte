ad1bb5ba6dd10732c3822309c8b62b7fbc5afbe0
SimplyAName <michael.loney137@gmail.com>
2024-03-15 20:00:34 +0000

    Try pcss again

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script>
-	import '../app.css';
+	import '../app.pcss';
 	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 	import { inject } from '@vercel/analytics';
 	import { dev } from '$app/environment';
